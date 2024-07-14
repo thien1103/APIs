ab4c142616b2bc4e167eac37e536ef9c7717474e
thien1103 <mshine644@gmail.com>
2024-07-14 10:30:58 +0700

    Remove verifyToken for get User Avatar

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,10 +5,9 @@ const verifyToken = require('../middleware/verifyToken');
 
 router.get("/user/all", verifyToken, userController.GetAllUserInfo);
 router.get('/user/:userId',verifyToken, userController.GetUserInfo);
-router.get("/user/avatar/:filename", verifyToken, userController.GetUserAvatar);
+router.get("/user/avatar/:filename", userController.GetUserAvatar);
 router.put('/user/update/:userId',verifyToken, userController.UpdateUserInfo);
 router.put('/user/changeAvatar/:userId', verifyToken, userController.ChangeAvatar);
-router.post('/user/changePassword/:userId',verifyToken, userController.ChangePassword);
 
 
 module.exports = router;
